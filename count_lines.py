90cde0c15c6ea6668bca96ce48d77848ce51e539
Veron19 <50548659+Veron19@users.noreply.github.com>
2020-08-03 17:59:31 +0300

    numbers_of_line

--- count_lines.py
+++ count_lines.py
@@ -1,11 +1,6 @@
-#f=open('text1.txt', 'r')
-#f.read 
-#text=f.read()
 marker = 0
 f=open ('./text1.txt')
 for line in f:
 	marker +=1
 	print (marker,line)
-#print (text)
-#print ('hello Polina ')
 f.close
